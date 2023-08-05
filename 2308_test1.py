f9ac8810f706f943e0c6809eb6ffa9bf57ff97c3
hglab23 <141329940+hglab23@users.noreply.github.com>
2023-08-05 22:46:18 +0900

    Add files via upload

--- 2308_test1.py
+++ 2308_test1.py
@@ -1,5 +1,4 @@
 import pyodbc
-#import streamlit as st
 
 # 연결 정보를 설정합니다.
 server = 'database-1.c3oa5eibtea1.us-east-2.rds.amazonaws.com'
@@ -18,26 +17,12 @@ try:
 
     # 쿼리를 실행합니다.
     cursor = connection.cursor()
-    cursor.execute('SELECT wtdata FROM wt002')
+    cursor.execute('SELECT * FROM wt002')
 
     # 결과를 출력합니다.
-    #for row in cursor:
-    #    print(row)
-    
-    # st.bar_chart(view)
-    
-    # 결과(row)를 리스트에 저장합니다.
-    row_data_list = []
     for row in cursor:
-        row_data_list.append(row)  # 각 row를 리스트에 추가합니다.  
-    
-    # 쉼표를 제거한 새로운 리스트를 생성합니다.
-    c_data_list = [int(item[0]) for item in row_data_list]
-    
-    # st.bar_chart(c_data_list)
-    print("Row data list:", c_data_list)   
-    
-    
+        print(row)
+
 except Exception as e:
     # 연결이 실패할 경우 오류 내용을 출력합니다.
     print(f"Error: {e}")
